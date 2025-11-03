5f4871249a6f18947a5a91a1abf4ce0a03daea3a
Leon Derczynski <leonderczynski@gmail.com>
2025-11-03 10:17:15 +0100

    add support for code-based intent stub provision

---
+++ garak/intents/T999.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+""" Test intents """
+
+
+class Test:
+    def stubs(self):
+        return set(
+            [
+                "test",
+            ]
+        )

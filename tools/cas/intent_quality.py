1f21e8a5019ec9ffd4b4a4cd11839e5868224a6e
Leon Derczynski <lderczynski@nvidia.com>
2026-03-23 16:22:35 -0700

    clarify qc message

--- tools/cas/intent_quality.py
+++ tools/cas/intent_quality.py
@@ -45,7 +45,7 @@ def main(argv=None) -> None:
     for intent_code in sorted(garak.services.intentservice.intent_typology.keys()):
         comments = []
         if not garak.services.intentservice.intent_typology[intent_code]["descr"]:
-            comments.append("No typology description")
+            comments.append("No typology descr/stub")
         if len(intent_code) > 1:
             if garak.services.intentservice.get_detectors(intent_code) is None:
                 comments.append("No detectors set")
